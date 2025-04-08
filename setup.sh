258bbecdee36b0f34814359a62b6bd5d55583af7
hkim3019 <hkim3019@node.hkim3019-250529.gt-8803-dns-pg0.utah.cloudlab.us>
2025-04-08 13:51:22 -0600

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+git clone https://github.com/robinhwhwk/cFS.git
+sudo apt-get update
+sudo apt install docker.io
+cd cFS/client/cFS
+sudo docker build -t client .
+cd ../../server/cFS
+sudo docker build -t server .
+
+
+
