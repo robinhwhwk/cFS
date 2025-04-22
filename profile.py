810a3524bffd72e1b0b8e38cbae6728558b94f26
Robin Kim <88211687+robinhwhwk@users.noreply.github.com>
2025-04-22 13:27:44 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,10 +18,14 @@ pc = portal.Context()
 request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
-node = request.RawPC("node")
+node1 = request.RawPC("node1")
+node2 = request.RawPC("node2")
 
 # Change node type to ARM64
-node.hardware_type = "m400"
+node1.hardware_type = "m400"
+node2.hardware_type = "m400"
+
+link1 = request.Link(members = [node1,node2])
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
